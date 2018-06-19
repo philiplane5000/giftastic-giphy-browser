e5c70187fa623b51b3cf1e035fad5ec3281a5add
philtd5000 <philiptaft9@gmail.com>
2018-06-19 12:41:33 +0200

    buttons on press generate 9 giphy videos in viewer div

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,6 +10,8 @@ $('body').on('click', '.giphy-search-btn', function (event) {
 
     event.preventDefault();
 
+    clearViewer();
+
     let query = $(this).attr('data-attribute');
 
     let queryURL = generateQueryURL(query);
@@ -19,12 +21,47 @@ $('body').on('click', '.giphy-search-btn', function (event) {
     $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function(res){
-        console.log(res.data[0].images.fixed_height.url);
-        console.log(res.data[0].rating);
-    });
-
+    }).then(updateGiphyViewer)
   });
+
+function updateGiphyViewer(response){
+    
+    // console.log(res.data[0].images.fixed_height.url);
+    // console.log(res.data[0].rating);
+
+//**************************************************************************************************/
+  // Get from the form the number of results to display
+  // API doesn't have a "limit" parameter, so we have to do this ourselves
+  console.log(response);
+
+  let count = response.pagination.count;
+
+  console.log(count);
+
+  for (let i = 0; i < count; i++) {
+
+    let $target = $('#giphy-viewer');
+     
+    // let giphURL = response.data[i].images.fixed_height.url;
+    // console.log(giphURL);
+    // let giphRating = response.data[i].rating;
+    // console.log(giphRating);
+
+    let $giphyDiv = $('<div>').addClass('giph-div');
+
+    let $rating = $('<div>').html(`<h6>Rating:${response.data[i].rating} </h6>`);
+    let $giphy = $('<img>').attr('src', response.data[i].images.fixed_width.url);
+
+    $giphyDiv.append($rating).append($giphy);
+    $target.append($giphyDiv);
+
+  } /*end for loop*/
+//**************************************************************************************************/
+}
+
+function clearViewer() {
+    $('#giphy-viewer').empty();
+}
 
 function buttonGenerator(arr) {
     //function to generate button for array input
@@ -43,7 +80,7 @@ function generateQueryURL(query) {
     let baseURL = "https://api.giphy.com/v1/gifs/";
     let apikey = "PW1vGxATHQSLxVgoWYhQmuqL7Dq0V9K0";
     let searchFor = query;
-    let limitResultsTo = 5;
+    let limitResultsTo = 9;
 
     let queryURL = `${baseURL}search?api_key=${apikey}&q=${searchFor}&limit=${limitResultsTo}`;
     queryURL.toString();
