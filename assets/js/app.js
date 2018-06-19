799eca45b5a0eceda534864878569177367db36a
philtd5000 <philiptaft9@gmail.com>
2018-06-19 00:36:40 -0700

    basic site layout and buttonGenerator function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
     console.log("CONNECTED");
 })
+
+let sports = ["cricket", "football", "basketball", "lacrosse", "badminton", "field hockey", "surfing", "soccer", "tennis", "volleyball", "ice hockey"];
+
+buttonGenerator(sports);
+
+function buttonGenerator(arr) {
+    //function to generate button for array input
+    let $target = $('#sportsButtons');
+    //if statement to run same function for single string item or value needed(!)
+    arr.forEach(function(item){
+        let $button = $('<button>');
+        $button.text(`${item}`);
+        $button.addClass(`${item}`);
+        $target.append($button);
+    });
+}
+
